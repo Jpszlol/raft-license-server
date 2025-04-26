79b08bdfd3947b05a48d89830ec028c3385655da
Jpszlol <PrivateSkipperLOL@protonmail.com>
2025-04-26 17:36:21 -0500

    server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ let keys = JSON.parse(fs.readFileSync('keys.json'));
 
 // Duration lookup table
 const durations = {
-  "24h": 30 * 1000, // ⏳ 30 seconds for testing!
+  "24h": 24 * 60 * 60 * 1000,  // ✅ Real 24 hours
   "7d": 7 * 24 * 60 * 60 * 1000,
   "30d": 30 * 24 * 60 * 60 * 1000
 };
